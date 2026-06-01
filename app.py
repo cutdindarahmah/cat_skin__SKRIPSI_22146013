40cdab2cbf8f4f980102eed517f7d8d866c99aa2
cutdindarahmah <cutdindarahmah@gmail.com>
2026-06-01 16:42:37 +0700

    solusi 5

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from PIL import Image
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 # ==========================
 # CONFIG
